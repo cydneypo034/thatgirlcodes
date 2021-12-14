f9ff1de0bd3f789932e9d6fb73ada42deaa28c8e
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-13 23:33:49 -0500

    added resource routes for database and server.js

--- routes/api/ladies.js
+++ routes/api/ladies.js
@@ -1,4 +1,36 @@
 const express = require("express");
 const router = express.Router();
 
-const Ladies = require('../../')
+const Ladies = require('../../models/ladies.js');
+
+router.get('/', (req, res) => {
+    Ladies.find()
+    .then(ladies => res.json(ladies))
+    .catch(err => res.status(404).json({ noladies: 'They are not Available'}));
+});
+
+router.get('/:id', (req, res) => {
+    Ladies.findById(req.params.id)
+    .then(ladies => res.json(lady))
+    .catch(err => res.status(404).json({ nolady: 'She is not Available'}));
+});
+
+router.post('/', (req, res) => {
+    Ladies.create(req.body)
+    .then(lady => res.json({ msg: 'Lady Added!'}))
+    .catch(err => res.status(404).json({ error: 'Unable to Add'}));
+});
+
+router.put('/:id', (req, res) => {
+    Ladies.findByIdAndUpdate(req.params.id, req.body)
+    .then(lady => res.json({msg: 'Lady updated!'}))
+    .catch(err => res.status(404).json({ error: 'Unable to Update'}));
+});
+
+router.delete('/:id', (req, res) => {
+    Ladies.findByIdAndDelete()
+    .then(lady => res.json({ msg: 'Lady Deleted!'}))
+    .catch(err => res.status(404).json({ error: 'Nothing to Delete'}));
+});
+
+module.exports = router;
---
+++ routes/api/resources.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+
+const Resources = require('../../models/resources.js');
+
+router.get('/', (req, res) => {
+    Resources.find()
+    .then(resources => res.json(resources))
+    .catch(err => res.status(404).json({ noresources: 'They are not Available'}));
+});
+
+router.get('/:id', (req, res) => {
+    Resources.findById(req.params.id)
+    .then(resource => res.json(resource))
+    .catch(err => res.status(404).json({ nolady: 'She is not Available'}));
+});
+
+router.post('/', (req, res) => {
+    Resources.create(req.body)
+    .then(resource => res.json({ msg: 'Resource Added!'}))
+    .catch(err => res.status(404).json({ error: 'Unable to Add'}));
+});
+
+router.put('/:id', (req, res) => {
+    Resources.findByIdAndUpdate(req.params.id, req.body)
+    .then(resource => res.json({ msg: 'Resource Updated!'}))
+    .catch(err => res.status(404).json({ error: 'Unable to Update'}));
+});
+
+router.delete('/:id', (req, res) => {
+    Resources.findByIdAndDelete()
+    .then(resource => res.json({ msg: 'Resource Deleted!'}))
+    .catch(err => res.status(404).json({ error: 'Nothing to Delete'}));
+});
+
+module.exports = router;
