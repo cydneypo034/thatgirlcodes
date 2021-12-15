3f6874384cc785de15d45367acc82b9dfe7b75c6
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-14 23:03:23 -0500

    added multer code and routes for image upload

--- routes/api/ladies.js
+++ routes/api/ladies.js
@@ -2,11 +2,23 @@ const express = require("express");
 const router = express.Router();
 
 const Ladies = require('../../models/ladies.js');
+const upload = multer({ storage: storage });
 
 router.get('/', (req, res) => {
     Ladies.find()
     .then(ladies => res.json(ladies))
     .catch(err => res.status(404).json({ noladies: 'They are not Available'}));
+});
+
+router.get('/', (req, res) => {
+    Ladies.find({}, (err, items) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send('An error has occured', err);
+        } else {
+            res.render('imagesPage', {items: items});
+        }
+    });
 });
 
 router.get('/:id', (req, res) => {
@@ -19,6 +31,23 @@ router.post('/', (req, res) => {
     Ladies.create(req.body)
     .then(lady => res.json({ msg: 'Lady Added!'}))
     .catch(err => res.status(404).json({ error: 'Unable to Add'}));
+});
+
+router.post('/', upload.single('image'), (req, res, next) => {
+    const obj = {
+        photo: {
+            data: fs.readFileSync(path.join(__dirname + '/uploads/' + req.file.filename)),
+            type: 'image/png'
+        }
+    }
+    Ladies.create(obj, (err, item) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            res.redirect('/');
+        }
+    });
 });
 
 router.put('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const dotenv = require("dotenv");
 const app = express();
 const cors = require("cors");
 
+
+//database connection///
 dotenv.config();
 const url = process.env.REACT_APP_MONGO_URL
 const connectDB = async () => {
@@ -23,17 +25,30 @@ const connectDB = async () => {
 };
 
 connectDB();
-
 app.use(cors());
 
+//middleware//
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+//origin headers//
+//routes//
 const ladyRoutes = require('./routes/api/ladies.js');
 const resourceRoutes = require('./routes/api/resources.js')
 app.use('/api/ladies', ladyRoutes)
 app.use('/api/resources', resourceRoutes)
 
+//image storage//
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now())
+    }
+});
 
+//port//
 const port = process.env.PORT || 8000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
