f767aa74c660290c6bbf59e16476fbb6012d6a30
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-16 01:34:20 -0500

    added react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  render() {
+     return (
+        <div className="App">
+        
+        <Router>
+          <Route exact path="/" element={<ShowHome />}/>
+          <Route path='/ladies' element={<ShowLadies />}/>
+          <Route path="/one-lady/:id" element={<OneLady />} />
+          <Route path="/create-ladies" element={<CreateLadies />} />
+          <Route path="/edit-ladies" element={<EditLadies/>} />       
+          <Route path="/resources" element={<ShowResources />}/>
+          <Route path="/one-resource/:id" element={<OneResource />} />
+          <Route path="/create-resources" element={<CreateResources />} />
+          <Route path="edit-resources" element={<EditResources />} />
+          </Router>
+        </div>
+      );
+  }
 }
 
 export default App;
