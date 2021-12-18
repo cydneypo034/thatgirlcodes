3b57bd68e05ecf8e32084a4fb986f2afc35df0bd
Cydney Pollard <sincerelycydney087@gmail.com>
2021-12-17 23:49:11 -0500

    added routing for app.js components

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Routes, Route } from 'react-router-dom';
+import {BrowserRouter, BrowserRouter as Routes, Route } from 'react-router-dom';
 import ShowHome from './components/Home.js';
 import ShowLadies from './components/AllLadies.js';
 import OneLady from './components/OneLady.js';
@@ -14,46 +14,21 @@ import EditResources from './components/EditResource.js';
 class App extends React.Component{
   render() {
      return (
-        <div className="App">
+        <div>
+          <BrowserRouter>
           <Routes>
-            <Route exact path="/">
-            <ShowHome />
-            </Route>
-
-            <Route path='/ladies'>
-            <ShowLadies />
-            </Route>
-
-            <Route path="/one-lady/:id">
-            <OneLady />
-            </Route>
-
-            <Route path="/create-ladies">
-            <CreateLadies />
-              </Route>
-
-            <Route path="/edit-ladies">
-            <EditLadies/>
-              </Route>
-
-            <Route path="/resources">
-            <ShowResources />
-            </Route>
-
-            <Route path="/one-resource/:id">
-            <OneResource />
-            </Route>
-
-            <Route path="/create-resources">
-            <CreateResources />
-            </Route>
-
-            <Route path="edit-resources">
-            <EditResources />
-            </Route>
+            <Route path="/" element={<ShowHome />} />
+            <Route path='/ladies' element={<ShowLadies />}/>
+            <Route path="/one-lady/:id" element={<OneLady />} />
+            <Route path="/create-ladies" element={<CreateLadies />} />
+            <Route path="/edit-ladies" element={<EditLadies/>}/>  
+            <Route path="/resources" element={<ShowResources />}/>
+            <Route path="/one-resource/:id" element={<OneResource />}/>
+            <Route path="/create-resources" element={<CreateResources />}/>
+            <Route path="edit-resources" element={<EditResources />}/> 
           </Routes>
-        
-
+          </BrowserRouter>
+          
         </div>
       );
   }
